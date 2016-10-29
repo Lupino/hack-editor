e24460f1a1d574f643406d77db5eb5bd2ce5eddb
Lupino <lmjubuntu@gmail.com>
2016-10-29 22:41:10 +0800

    Add timeSuffix on readFile

--- fay/RFile.hs
+++ fay/RFile.hs
@@ -8,18 +8,24 @@ module RFile
     deleteFile
   ) where
 
-import           Data.Text (Text, fromString)
+import           Data.Text (Text, fromString, (<>))
+import           FFI       (ffi)
 import           FilePath  (FilePath, (</>))
 import           FPromise  (Promise, then_)
 import           HTTP      (delete, get, put, resolveText)
 import           Prelude
+
+timeSuffix :: Fay Text
+timeSuffix = ffi "(function() {return '?t=' + Math.floor(new Date() / 1000 )})()"
 
 saveFile :: FilePath -> Text -> Fay Promise
 saveFile fn body = put url (Just body) >>= then_ resolveText
   where url = "/api/file" </> fn
 
 readFile :: FilePath -> Fay Promise
-readFile fn = get url >>= then_ resolveText
+readFile fn = do
+  t <- timeSuffix
+  get (url <> t) >>= then_ resolveText
   where url = "/api/file" </> fn
 
 deleteFile :: FilePath -> Fay Promise
